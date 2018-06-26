260393f9a0bf3364da0108c4e3a8f9b40cfdc79d
ProgrammerColton <40554043+ProgrammerColton@users.noreply.github.com>
2018-06-26 09:37:07 -0700

    Update rbxthemeparkapi.lua

--- roblox/rbxthemeparkapi.lua
+++ roblox/rbxthemeparkapi.lua
@@ -1,1 +1,39 @@
+--[[
+ __                                   __                  
+|__)  _  _   _   _  _   _   _   _  _ /    _  | |_  _   _  
+|    |  (_) (_) |  (_| ||| ||| (- |  \__ (_) | |_ (_) | ) 
+            _/
+__________________________________________________________
+
+--]]
+
+local RbxThemeparkAPI = {}
+
+--// Variables:
+local httpService = game:GetService("HttpService");
+
+function RbxThemeparkAPI:Init(url, token)
+	self.url = url;
+	self.token = token;
+	return self;
+end
+
+function RbxThemeparkAPI:Get(path)
+	local data = {
+		["token"] = self.token;
+	}
+	if not(path) then
+		return "Error: Path null."
+	end
+	local result;
+	local success, err = pcall(function()
+		result = httpService:GetAsync(self.url..path, false, data);
+	end)
+	if not(success) then
+		return ("Error: " .. err);
+	end
+	return httpService:JSONDecode(result);
+end
+
+return RbxThemeparkAPI
 
